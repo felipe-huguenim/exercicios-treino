899508509e9e88334994f52598db3ec960f7fb00
Felipe Oliveira <94530473+FelipeHOdev@users.noreply.github.com>
2024-09-09 21:15:53 -0300

    Update Exerc1-Conta restaurante.py
    comentários adicionados para melhor compreensão

--- Exerc1-Conta restaurante.py
+++ Exerc1-Conta restaurante.py
@@ -1,3 +1,6 @@
+#Inserir valor do produto
+#Adicionar a taxa de serviço
+#Mostrar o valor total com a taxa de serviço adicionada
 while True:
   valor = int(input("Valor do pedido: "))
   taxa = valor * 10 / 100
